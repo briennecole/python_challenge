c841844062af32086caaab585b97f20f75848ecb
Brienne <cole.brienne@gmail.com>
2021-05-22 22:30:15 -0700

    calculations not correct

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,6 +6,18 @@ import csv
 #Path to collect data from CSV file
 budget_csv = os.path.join('Resources', 'budget_data.csv')
 
+# Define Variables
+month_total = 0
+net_revenue = 0
+greatest_increase = 0
+greatest_decrease = 0
+increase_month = ""
+decrease_month = "" 
+monthly_change = 0
+previous_month = 0
+difference = 0
+
+
 # Read the csv file
 with open (budget_csv) as csv_file:
 
@@ -16,16 +28,19 @@ with open (budget_csv) as csv_file:
     header = next(csvreader)
 
         # Loop through the data
-    num_months = 0
     for row in csvreader:
 
         #Count the number of months
-        num_months = num_months + 1
-        print (num_months)
+        month_total = month_total + 1
+        #print (month_total)
 
         # Find sum of P & L
-        net_pandl = sum([int(row[1])])
+        net_revenue = sum([int(row[1])])
         #print (net_pandl)
+
+        # Calculate Average P&L monthly change
+        avg_change = (net_revenue / month_total)
+        #print(avg_change)
 
 
 #def profit_loss(budget_data)
@@ -33,6 +48,8 @@ with open (budget_csv) as csv_file:
     # Name Calculations
     # Total Months = sum(budget_data[0])
     # Net Profit/Loss
+
+
 
 
 #Output Format
