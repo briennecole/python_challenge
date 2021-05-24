9c42bb14c742208c8f8b7f2fe59f12a0a4bf0c14
Brienne <cole.brienne@gmail.com>
2021-05-24 15:56:25 -0700

    struggling with average change..

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,14 +8,16 @@ budget_csv = os.path.join('Resources', 'budget_data.csv')
 
 # Define Variables
 month_total = 0
-net_revenue = 0
+net_rev = 0
 greatest_increase = 0
 greatest_decrease = 0
 increase_month = ""
 decrease_month = "" 
+current_rev = 0
+previous_rev = 0
 monthly_change = 0
-previous_month = 0
 difference = 0
+change = []
 
 
 # Read the csv file
@@ -33,31 +35,35 @@ with open (budget_csv) as csv_file:
         #Count the number of months
         month_total = month_total + 1
         #print (month_total)
+        
+        # Calculate net revenue
+        net_rev = float(net_rev) + float(row[1])
+
+        # Calculate monthly profit change
+        if (month_total == 1):
+            previous_rev = int(row[1])
+
+        else:
+            current_rev = int(row[1])
+            previous_rev = int(row[1])
+            monthly_change = current_rev - previous_rev
+            change.append(monthly_change)
+            
 
         # Find sum of P & L
-        net_revenue = sum([int(row[1])])
-        #print (net_pandl)
+        # print (net_pandl)
 
-        # Calculate Average P&L monthly change
-        avg_change = (net_revenue / month_total)
+# Calculate Average change
+avg_change = sum(change) / len(change)
         #print(avg_change)
 
 
-#def profit_loss(budget_data)
-
-    # Name Calculations
-    # Total Months = sum(budget_data[0])
-    # Net Profit/Loss
-
-
-
-
 #Output Format
-# print("Financial Analysis")
-# print("-------------------------------------")
-# print (f"Total Months: {month_total}")
-# print (f"Total: ${net_total}")
-# print (f"Average Change: ${avg_change}")
+print("Financial Analysis")
+print("-------------------------------------")
+print (f"Total Months: {month_total}")
+print (f"Total: ${round(net_rev)}")
+print (f"Average Change: ${avg_change}")
 # print (f"Greatest Increase in Profits: {}")
 # print (f"Greatest Decrease in Profits: {}")
 
