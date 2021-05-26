90cd5fc2b9d4bb4b4901996de74cdbdcce2ea9fa
Brienne <cole.brienne@gmail.com>
2021-05-25 19:50:57 -0700

    user git error

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -25,9 +25,39 @@ with open(votes) as v:
         vote_total += 1
 
 # Print Election Results
+print("Election Results")
+print("----------------------------")
+print(f"Total Votes: {vote_total}")
+print("----------------------------")
 
+# Perform and print vote percentage calculations
+for candidate in votes:
+    percent_total = votes[candidate]/vote_total
+    percentage = "{:.3%}".format(percent_total)
+    print(f"{candidate}. {percentage} ({votes[candidate]}")
 
+# Print Winner
+print("----------------------------")    
+print(f"Winner: {votes.most_common(1)[0][0]}" )
 
+print("----------------------------")
+
+# Write to text file
+file = open('Analysis/election_results.txt', "w")
+file.write("Election Results\n")
+file.write("----------------------------\n")
+file.write("Total Votes: " + str(vote_total) + "\n")
+file.write("----------------------------\n")
+
+for candidate in votes:
+    percent_total = votes[candidate]/vote_total
+    percentage = "{:.3%}".format(percent_total)
+    file.write(candidate + ": " + str(percentage) + "("  + str(votes[candidate]) + ")\n")
+
+file.write("-----------------------------\n")
+file.write(f"Winner: {votes.most_common(1)[0][0]}\n")
+file.write("-----------------------------\n")
+file.close()
 
 
 
