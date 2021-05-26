55684e28c01288ff88439e8dce1a0c4a6e27653f
Brienne <cole.brienne@gmail.com>
2021-05-25 19:25:00 -0700

    fixing errors on pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -24,37 +24,10 @@ with open(votes) as v:
         votes[candidate] += 1
         vote_total += 1
 
-# Print election results
-print("Election Results")
-print("----------------------------")
-print(f"Total Votes: {vote_total}")
-print("----------------------------")
+# Print Election Results
 
-# Perform and print vote percentage calculations
-for candidate in votes:
-    percent_total = votes[candidate]/vote_total
-    percentage = "{:.3%}".format(percent_total)
-    print(f"{candidate}: {percentage}  ({votes[candidate]})")
 
-# print Winner
-print("----------------------------")    
-print(f"Winner: {votes.most_common(1)[0][0]}" )
 
-print("----------------------------")
 
-# Write to text file
-file = open('Analysis/election_results.txt', "w")
-file.write("Election Results\n")
-file.write("----------------------------\n")
-file.write("Total Votes: " + str(vote_total) + "\n")
-file.write("----------------------------\n")
 
-for candidate in votes:
-    percent_total = votes[candidate]/vote_total
-    percentage = "{:.3%}".format(percent_total)
-    file.write(candidate + ": " + str(percentage) + "("  + str(votes[candidate]) + ")\n")
 
-file.write("-----------------------------\n")
-file.write(f"Winner: {votes.most_common(1)[0][0]}\n")
-file.write("-----------------------------\n")
-file.close()
